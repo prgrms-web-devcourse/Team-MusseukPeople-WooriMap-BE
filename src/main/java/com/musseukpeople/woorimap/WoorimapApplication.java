f3c04294745942d719ec0f37ff7806c9b11b255c
yuminhwan <ymh7532@icloud.com>
2022-07-24 14:40:08 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/musseukpeople/woorimap/WoorimapApplication.java
@@ -0,0 +1,13 @@
+package com.musseukpeople.woorimap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorimapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoorimapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musseukpeople/woorimap/WoorimapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musseukpeople.woorimap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorimapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
