a62882fa4c1a6c4c12edd1a9a930d3f9014e98ea
yuminhwan <ymh7532@icloud.com>
2022-07-27 00:18:20 +0900

    [WOOR-67] chore: swagger 설정

--- src/main/java/com/musseukpeople/woorimap/HealthCheckController.java
+++ src/main/java/com/musseukpeople/woorimap/HealthCheckController.java
@@ -3,11 +3,17 @@ package com.musseukpeople.woorimap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "헬스 체크", description = "헬스 체크 API입니다.")
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/health")
-    public String check() {
-        return "health!!!";
-    }
+	@Operation(summary = "헬스 체크", description = "서버 실행 여부를 확인합니다.")
+	@GetMapping("/health")
+	public String check() {
+		return "Check This Sound";
+	}
+
 }
