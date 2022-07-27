ee362c2a5b5f6bce6f5677a61a8fe19319a76f05
yuminhwan <ymh7532@icloud.com>
2022-07-27 16:47:43 +0900

    [WOOR-47] chore: AcceptanceTest,DatabaseCleanup 클래스 생성

---
+++ src/test/java/com/musseukpeople/woorimap/util/AcceptanceTest.java
@@ -0,0 +1,28 @@
+package com.musseukpeople.woorimap.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class AcceptanceTest {
+
+	@Autowired
+	protected MockMvc mockMvc;
+
+	@Autowired
+	protected ObjectMapper objectMapper;
+
+	@Autowired
+	private DatabaseCleanup databaseCleanup;
+
+	@AfterEach
+	void tearDown() {
+		databaseCleanup.execute();
+	}
+}
