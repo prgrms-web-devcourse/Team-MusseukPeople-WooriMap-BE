d3b9b0c5ffc67cd01c4c86a93d77e29f93682e90
yuminhwan <ymh7532@icloud.com>
2022-07-27 23:36:53 +0900

    [WOOR-47] feat: 닉네임 객체 구현

---
+++ src/main/java/com/musseukpeople/woorimap/member/domain/NickName.java
@@ -0,0 +1,35 @@
+package com.musseukpeople.woorimap.member.domain;
+
+import static com.google.common.base.Preconditions.*;
+import static java.text.MessageFormat.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NickName {
+
+	private static final int MAX_NICKNAME_LENGTH = 15;
+
+	@Column(name = "nickname", nullable = false)
+	private String value;
+
+	public NickName(String value) {
+		validateNickName(value);
+		this.value = value;
+	}
+
+	private void validateNickName(String value) {
+		checkArgument(!StringUtils.isBlank(value), format("닉네임은 비어있을 수 없습니다. nickName = {0}", value));
+		checkArgument(value.length() <= MAX_NICKNAME_LENGTH,
+			format("닉네임은 {0}글자를 초과할 수 없습니다. nickNameLength = {1}", MAX_NICKNAME_LENGTH, value.length()));
+	}
+}
