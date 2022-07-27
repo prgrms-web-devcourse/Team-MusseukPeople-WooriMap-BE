220a11203e8deea6a3e08fad98a0f04f055faed0
Yongsu Kang <dksktm15@naver.com>
2022-07-28 00:33:41 +0900

    [WOOR-79] refac: ErrorResponse 불필요 메소드 제거 및 final 수정

--- src/main/java/com/musseukpeople/woorimap/common/exception/ErrorResponse.java
+++ src/main/java/com/musseukpeople/woorimap/common/exception/ErrorResponse.java
@@ -1,22 +1,22 @@
 package com.musseukpeople.woorimap.common.exception;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.validation.BindingResult;
 
-import lombok.AccessLevel;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ErrorResponse {
 
-	private String message;
-	private String code;
-	private List<FieldError> errors;
+	private final String message;
+	private final String code;
+
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private final List<FieldError> errors;
 
 	private ErrorResponse(ErrorCode code, List<FieldError> errors) {
 		this.message = code.getMessage();
@@ -32,32 +32,20 @@ public class ErrorResponse {
 		return new ErrorResponse(code, null);
 	}
 
-	public static ErrorResponse of(ErrorCode code, List<FieldError> errors) {
-		return new ErrorResponse(code, errors);
-	}
-
 	@Getter
-	@NoArgsConstructor(access = AccessLevel.PROTECTED)
 	public static class FieldError {
-		private String field;
-		private String value;
-		private String reason;
+		private final String field;
+		private final String value;
+		private final String reason;
 
-		private FieldError(final String field, final String value, final String reason) {
+		private FieldError(String field, String value, String reason) {
 			this.field = field;
 			this.value = value;
 			this.reason = reason;
 		}
 
-		public static List<FieldError> of(final String field, final String value, final String reason) {
-			List<FieldError> fieldErrors = new ArrayList<>();
-			fieldErrors.add(new FieldError(field, value, reason));
-			return fieldErrors;
-		}
-
 		private static List<FieldError> of(final BindingResult bindingResult) {
-			final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
-			return fieldErrors.stream()
+			return bindingResult.getFieldErrors().stream()
 				.map(error -> new FieldError(
 					error.getField(),
 					error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
