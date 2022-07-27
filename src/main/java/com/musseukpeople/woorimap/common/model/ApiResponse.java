ab6969a273ff38ec7bc6ec3dd28488967e6a016c
강용수 <97223877+yongsu-kang@users.noreply.github.com>
2022-07-27 15:53:08 +0900

    [WOOR-77] feat: ApiResponse 구현 (#4)
    [WOOR-77] feat: ApiResponse 구현

---
+++ src/main/java/com/musseukpeople/woorimap/common/model/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.musseukpeople.woorimap.common.model;
+
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+
+	private final T data;
+
+	public ApiResponse(T data) {
+		this.data = data;
+	}
+}
