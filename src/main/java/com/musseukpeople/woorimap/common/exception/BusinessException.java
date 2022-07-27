b99fb9479ca6d07f41a82b70239f4ca90401841f
Yongsu Kang <dksktm15@naver.com>
2022-07-27 22:42:14 +0900

    [WOOR-79] feat: BusinessException 생성

---
+++ src/main/java/com/musseukpeople/woorimap/common/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.musseukpeople.woorimap.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+	private final ErrorCode errorCode;
+
+	public BusinessException(String message, ErrorCode errorCode) {
+		super(message);
+		this.errorCode = errorCode;
+	}
+}
