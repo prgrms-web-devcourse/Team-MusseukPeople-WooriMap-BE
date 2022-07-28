b645a68d7fece0b5cee5d971bb3c3b681c2a3a14
yuminhwan <ymh7532@icloud.com>
2022-07-28 14:48:56 +0900

    [WOOR-47] refactor: AccessDeniedException 삭제 및 IllegalArgumentException 처리할 수 있도록 리팩토링

--- src/main/java/com/musseukpeople/woorimap/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/musseukpeople/woorimap/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.musseukpeople.woorimap.common.exception;
 
-import java.nio.file.AccessDeniedException;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,15 +22,6 @@ public class GlobalExceptionHandler {
 			.body(ErrorResponse.of(errorCode));
 	}
 
-	@ExceptionHandler(AccessDeniedException.class)
-	public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
-		log.error(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage(), e);
-		ErrorCode errorCode = ErrorCode.HANDLE_ACCESS_DENIED;
-		return ResponseEntity
-			.status(errorCode.getStatus())
-			.body(ErrorResponse.of(errorCode));
-	}
-
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
 		log.error(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage(), e);
@@ -40,6 +29,15 @@ public class GlobalExceptionHandler {
 		return ResponseEntity
 			.status(errorCode.getStatus())
 			.body(ErrorResponse.of(errorCode, e.getBindingResult()));
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+		log.warn(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage(), e);
+		ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+		return ResponseEntity
+			.status(errorCode.getStatus())
+			.body(ErrorResponse.of(errorCode, e.getMessage()));
 	}
 
 	@ExceptionHandler(BusinessException.class)
