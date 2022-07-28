91be477056f56be4fd65170783951097f4936d57
yuminhwan <ymh7532@icloud.com>
2022-07-28 15:26:58 +0900

    [WOOR-47] refactor: 문자열 valid NotBlank로 변경

--- src/main/java/com/musseukpeople/woorimap/member/application/dto/request/SignupRequest.java
+++ src/main/java/com/musseukpeople/woorimap/member/application/dto/request/SignupRequest.java
@@ -1,7 +1,7 @@
 package com.musseukpeople.woorimap.member.application.dto.request;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,13 +12,13 @@ import lombok.NoArgsConstructor;
 public class SignupRequest {
 
 	@Email(message = "올바른 이메일 형식이 아닙니다.")
-	@NotNull(message = "이메일은 비어있을 수 없습니다.")
+	@NotBlank(message = "이메일은 비어있을 수 없습니다.")
 	private String email;
 
-	@NotNull(message = "비밀번호는 비어있을 수 없습니다.")
+	@NotBlank(message = "비밀번호는 비어있을 수 없습니다.")
 	private String password;
 
-	@NotNull(message = "닉네임은 비어있을 수 없습니다.")
+	@NotBlank(message = "닉네임은 비어있을 수 없습니다.")
 	private String nickName;
 
 	public SignupRequest(String email, String password, String nickName) {
