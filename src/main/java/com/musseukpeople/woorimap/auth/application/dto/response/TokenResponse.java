27e9dc3760ef4b88fa6e5ba3eb574268a971d647
yuminhwan <ymh7532@icloud.com>
2022-07-28 16:59:45 +0900

    [WOOR-53] feat: 로그인 응답 DTO 구현

---
+++ src/main/java/com/musseukpeople/woorimap/auth/application/dto/response/TokenResponse.java
@@ -0,0 +1,29 @@
+package com.musseukpeople.woorimap.auth.application.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class TokenResponse {
+
+	private final String accessToken;
+	private final String refreshToken;
+	private final MemberResponse memberResponse;
+
+	public TokenResponse(String accessToken, String refreshToken, MemberResponse memberResponse) {
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+		this.memberResponse = memberResponse;
+	}
+
+	@Getter
+	public static class MemberResponse {
+
+		private final String email;
+		private final String nickName;
+
+		public MemberResponse(String email, String nickName) {
+			this.email = email;
+			this.nickName = nickName;
+		}
+	}
+}
