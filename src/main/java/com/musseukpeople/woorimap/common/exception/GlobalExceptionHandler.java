96c1d49dc8014fca0a732ec5c568a734ca7d5c92
yuminhwan <ymh7532@icloud.com>
2022-07-28 15:27:34 +0900

    [WOOR-47] feat: HttpRequestMethodNotSupportedException 예외 처리

--- src/main/java/com/musseukpeople/woorimap/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/musseukpeople/woorimap/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.musseukpeople.woorimap.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,10 +27,17 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
 		log.error(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage(), e);
-		ErrorCode errorCode = ErrorCode.METHOD_NOT_ALLOWED;
+		ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
 		return ResponseEntity
 			.status(errorCode.getStatus())
 			.body(ErrorResponse.of(errorCode, e.getBindingResult()));
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<ErrorResponse> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+		log.error(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage(), e);
+		ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
+		return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
 	}
 
 	@ExceptionHandler(IllegalArgumentException.class)
