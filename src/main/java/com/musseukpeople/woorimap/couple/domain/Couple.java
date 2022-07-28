a6ab01ebb0ba8655ee6dad8a0bec4854fa100bf6
Yongsu Kang <dksktm15@naver.com>
2022-07-28 16:39:09 +0900

    [WOOR-80] feat: 커플 엔티티 생성

---
+++ src/main/java/com/musseukpeople/woorimap/couple/domain/Couple.java
@@ -0,0 +1,35 @@
+package com.musseukpeople.woorimap.couple.domain;
+
+import static com.google.common.base.Preconditions.*;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Couple {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate startDate;
+
+	public Couple(LocalDate startDate) {
+		checkArgument(
+			startDate.isBefore(LocalDate.now().plusDays(1)),
+			"현재 이후 날짜로 커플을 생성할 수 없습니다.");
+		this.startDate = startDate;
+	}
+}
