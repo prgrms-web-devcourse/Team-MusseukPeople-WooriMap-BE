a4c532d93754e7d2406eae89caffe5a97bda397f
Yongsu Kang <dksktm15@naver.com>
2022-07-28 16:39:28 +0900

    [WOOR-80] feat: 커플 repo 생성

---
+++ src/main/java/com/musseukpeople/woorimap/couple/domain/CoupleRepository.java
@@ -0,0 +1,6 @@
+package com.musseukpeople.woorimap.couple.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoupleRepository extends JpaRepository<Couple, Long> {
+}
