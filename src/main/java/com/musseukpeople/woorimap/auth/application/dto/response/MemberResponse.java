8d23575553da0cbbc7404c6b9cc5743fca0b21a5
yuminhwan <ymh7532@icloud.com>
2022-07-29 18:06:24 +0900

    [WOOR-110] feat: 로그인 API Swaager 적용

--- src/main/java/com/musseukpeople/woorimap/auth/application/dto/response/MemberResponse.java
+++ src/main/java/com/musseukpeople/woorimap/auth/application/dto/response/MemberResponse.java
@@ -1,5 +1,6 @@
 package com.musseukpeople.woorimap.auth.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberResponse {
 
+    @Schema(description = "이메일")
     private String email;
+
+    @Schema(description = "닉네임")
     private String nickName;
 
     public MemberResponse(String email, String nickName) {
--- src/main/java/com/musseukpeople/woorimap/auth/application/dto/response/TokenResponse.java
+++ src/main/java/com/musseukpeople/woorimap/auth/application/dto/response/TokenResponse.java
@@ -1,5 +1,6 @@
 package com.musseukpeople.woorimap.auth.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TokenResponse {
 
+    @Schema(description = "액세스 토큰")
     private String accessToken;
+
+    @Schema(description = "리프레시 토큰")
     private String refreshToken;
+
+    @Schema(description = "멤버 정보")
     private MemberResponse member;
 
     public TokenResponse(String accessToken, String refreshToken, MemberResponse memberResponse) {
