2c72570e4230ca9e68088ffefb3f47f3c6358303
yuminhwan <ymh7532@icloud.com>
2022-07-29 22:49:04 +0900

    [WOOR-54] feat: 회원아이디와 커플아이디 바인딩 해주는 resolver 구현

--- src/main/java/com/musseukpeople/woorimap/common/config/WebConfig.java
+++ src/main/java/com/musseukpeople/woorimap/common/config/WebConfig.java
@@ -1,5 +1,16 @@
 package com.musseukpeople.woorimap.common.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.musseukpeople.woorimap.auth.application.JwtProvider;
+import com.musseukpeople.woorimap.auth.presentation.AuthArgumentResolver;
+import com.musseukpeople.woorimap.auth.presentation.AuthInterceptor;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +22,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor(jwtProvider))
+            .addPathPatterns("/api/**")
+            .excludePathPatterns("/api/signin", "/api/members/signup");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthArgumentResolver(jwtProvider));
     private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     @Override
