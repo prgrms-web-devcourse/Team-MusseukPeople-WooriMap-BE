92f3d2bf99bbaef5bfa6bf12fa4ab77c800cbd8d
yuminhwan <ymh7532@icloud.com>
2022-07-29 16:21:34 +0900

    [WOOR-53] refactor: 오타 수정

--- src/main/java/com/musseukpeople/woorimap/member/exception/NotFoundMemberException.java
+++ src/main/java/com/musseukpeople/woorimap/member/exception/NotFoundMemberException.java
@@ -6,7 +6,7 @@ import com.musseukpeople.woorimap.common.exception.ErrorCode;
 
 public class NotFoundMemberException extends MemberException {
 
-    private static final String ERROR_MESSAGE_FORMAT = "존재하지 않는 회원 이메일입니다. 회원 번호: {0}";
+    private static final String ERROR_MESSAGE_FORMAT = "존재하지 않는 회원입니다. 회원 번호: {0}";
 
     public NotFoundMemberException(ErrorCode errorCode, Long id) {
         super(format(ERROR_MESSAGE_FORMAT, id), errorCode);
