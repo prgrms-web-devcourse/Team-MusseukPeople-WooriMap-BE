72ee1bc0da108038f8c543fb41216bd8db8e9ad3
dbslzld15 <dbslzld15@naver.com>
2022-07-30 18:28:54 +0900

    [WOOR-41] chore: cloud watch 연동 구현

---
+++ src/main/java/com/musseukpeople/woorimap/common/config/AwsConfig.java
@@ -0,0 +1,34 @@
+package com.musseukpeople.woorimap.common.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    private AWSCredentials awsCredentials;
+
+    @PostConstruct
+    public void init() {
+        awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(awsCredentials);
+    }
+
+}
