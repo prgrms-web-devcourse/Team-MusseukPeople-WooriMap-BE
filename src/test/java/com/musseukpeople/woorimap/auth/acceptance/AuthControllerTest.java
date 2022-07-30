a7b5d82932800d47dccdaa41db17983411cfa6e0
yuminhwan <ymh7532@icloud.com>
2022-07-30 15:55:03 +0900

    [WOOR-33] test: 패키지 구조 변경

--- src/test/java/com/musseukpeople/woorimap/auth/acceptance/AuthControllerTest.java
+++ src/test/java/com/musseukpeople/woorimap/auth/acceptance/AuthControllerTest.java
@@ -1,4 +1,4 @@
-package com.musseukpeople.woorimap.auth.presentation;
+package com.musseukpeople.woorimap.auth.acceptance;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
