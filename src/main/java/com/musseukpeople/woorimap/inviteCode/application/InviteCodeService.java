7c71715174140266b72d3897f57410d43a14fbd2
Yongsu Kang <dksktm15@naver.com>
2022-07-30 16:46:51 +0900

    [WOOR-34] feat: inviteCode 생성 기능 구현

---
+++ src/main/java/com/musseukpeople/woorimap/inviteCode/application/InviteCodeService.java
@@ -0,0 +1,38 @@
+package com.musseukpeople.woorimap.inviteCode.application;
+
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.musseukpeople.woorimap.inviteCode.application.dto.response.InviteCodeResponse;
+import com.musseukpeople.woorimap.inviteCode.domain.InviteCode;
+import com.musseukpeople.woorimap.inviteCode.domain.InviteCodeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class InviteCodeService {
+
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    private final InviteCodeRepository inviteCodeRepository;
+
+    @Transactional
+    public InviteCodeResponse createInviteCode(Long inviterId) {
+        return new InviteCodeResponse(getInviteCode(inviterId).orElse(
+            inviteCodeRepository.save(new InviteCode(createRandomInviterCode(), inviterId))).getCode());
+    }
+
+    private Optional<InviteCode> getInviteCode(Long inviterId) {
+        return inviteCodeRepository.findInviteCodeByInviterId(inviterId);
+    }
+
+    private String createRandomInviterCode() {
+        String randomInviteCodeNumber = String.valueOf(random.nextInt(10000000, 100000000));
+        return randomInviteCodeNumber.substring(randomInviteCodeNumber.length() - 7);
+    }
+}
