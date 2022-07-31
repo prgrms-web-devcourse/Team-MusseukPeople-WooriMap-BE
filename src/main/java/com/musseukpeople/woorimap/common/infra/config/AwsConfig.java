34372cb24630a1cd4576872ca619dfbb6123e492
ParkWooJin <47207658+dbslzld15@users.noreply.github.com>
2022-07-31 17:11:19 +0900

    [WOOR-41] chore: cloud watch 연동 구현 (#18)
    * [WOOR-41] chore: cloud watch 연동 구현
    
    * [WOOR-41] chore: cloudwatch log prefix 수정
    
    * chore: 서브모듈 업데이트
    
    * [WOOR-41] feat: AwsConfig 프로파일 설정
    
    * [WOOR-41] chore: aws 관련 설정 추가

---
+++ src/main/java/com/musseukpeople/woorimap/common/infra/config/AwsConfig.java
@@ -0,0 +1,36 @@
+package com.musseukpeople.woorimap.common.infra.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+
+@Profile("!test")
+@Configuration
+public class AwsConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    private AWSCredentials awsCredentials;
+
+    @PostConstruct
+    public void init() {
+        awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(awsCredentials);
+    }
+
+}
