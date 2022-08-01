f0cd331ea71e0239d3e55ea4271a39b92472c782
Yongsu Kang <dksktm15@naver.com>
2022-08-01 15:05:44 +0900

    [WOOR-34] refactor: limited code number 으로 하드코딩 수정

--- src/main/java/com/musseukpeople/woorimap/couple/infrastructure/RandomCodeGenerator.java
+++ src/main/java/com/musseukpeople/woorimap/couple/infrastructure/RandomCodeGenerator.java
@@ -10,10 +10,11 @@ import com.musseukpeople.woorimap.couple.application.CodeGenerator;
 public class RandomCodeGenerator implements CodeGenerator {
 
     private static final SecureRandom random = new SecureRandom();
+    private static final int LIMITED_CODE_NUMBER = 10000000;
 
     @Override
     public String createRandomCode() {
-        String randomInviteCodeNumber = String.valueOf(random.nextInt(10000000) + 10000000);
+        String randomInviteCodeNumber = String.valueOf(random.nextInt(LIMITED_CODE_NUMBER) + LIMITED_CODE_NUMBER);
         return randomInviteCodeNumber.substring(randomInviteCodeNumber.length() - 7);
     }
 }
