5ba3cc3e0c6d8d33a7b72b363b42a8fbf0ea0f2d
Yongsu Kang <dksktm15@naver.com>
2022-08-01 14:27:28 +0900

    [WOOR-34] refactor: @Repository 어노태이션 제거

--- src/main/java/com/musseukpeople/woorimap/invitecode/infrastructure/InviteCodQueryRepositoryImpl.java
+++ src/main/java/com/musseukpeople/woorimap/invitecode/infrastructure/InviteCodQueryRepositoryImpl.java
@@ -5,15 +5,12 @@ import static com.musseukpeople.woorimap.invitecode.domain.QInviteCode.*;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import org.springframework.stereotype.Repository;
-
 import com.musseukpeople.woorimap.invitecode.domain.InviteCode;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
-@Repository
 @RequiredArgsConstructor
 public class InviteCodQueryRepositoryImpl implements InviteCodQueryRepository {
 
