0ac88c6dc1d684a421650b2af6281e6c0d5271b2
Yongsu Kang <dksktm15@naver.com>
2022-08-02 12:01:43 +0900

    [WOOR-69] refactor: Member 서비스 breakUp 기능 추가

--- src/main/java/com/musseukpeople/woorimap/couple/application/CoupleFacade.java
+++ src/main/java/com/musseukpeople/woorimap/couple/application/CoupleFacade.java
@@ -3,23 +3,30 @@ package com.musseukpeople.woorimap.couple.application;
 import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.musseukpeople.woorimap.couple.application.dto.response.InviteCodeResponse;
+import com.musseukpeople.woorimap.member.application.MemberService;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CoupleFacade {
 
+    private final MemberService memberService;
     private final CoupleService coupleService;
     private final InviteCodeService inviteCodeService;
 
+    @Transactional
     public InviteCodeResponse createInviteCode(Long inviterId, LocalDateTime expireDate) {
         return inviteCodeService.createInviteCode(inviterId, expireDate);
     }
 
+    @Transactional
     public void removeCouple(Long coupleId) {
+        memberService.breakUpMembersByCoupleId(coupleId);
         coupleService.removeCouple(coupleId);
     }
 }
