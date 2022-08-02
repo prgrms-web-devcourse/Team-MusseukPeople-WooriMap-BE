5ed5c89bc0c8f252fbeb66a103aecbccaa319721
Yongsu Kang <dksktm15@naver.com>
2022-08-02 22:12:55 +0900

    [WOOR-70] feat: 커플 생성 기능 구현

--- src/main/java/com/musseukpeople/woorimap/couple/application/CoupleFacade.java
+++ src/main/java/com/musseukpeople/woorimap/couple/application/CoupleFacade.java
@@ -1,12 +1,18 @@
 package com.musseukpeople.woorimap.couple.application;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.musseukpeople.woorimap.auth.domain.login.LoginMember;
+import com.musseukpeople.woorimap.common.exception.ErrorCode;
 import com.musseukpeople.woorimap.couple.application.dto.response.InviteCodeResponse;
+import com.musseukpeople.woorimap.couple.exception.CreateCoupleFailException;
 import com.musseukpeople.woorimap.member.application.MemberService;
+import com.musseukpeople.woorimap.member.domain.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,12 +26,30 @@ public class CoupleFacade {
     private final InviteCodeService inviteCodeService;
 
     @Transactional
+    public void createCouple(String inviteCode, LoginMember receiver) {
+        Long receiverId = receiver.getId();
+        Long inviterId = inviteCodeService.getInviterIdByInviteCode(inviteCode);
+
+        if (Objects.equals(inviterId, receiverId)) {
+            throw new CreateCoupleFailException("자기 자신이 커플을 맺을 수 없습니다.", ErrorCode.NOT_CREATE_COUPLE);
+        }
+
+        Member foundInviter = memberService.getMemberById(inviterId);
+        Member foundReceiver = memberService.getMemberById(receiverId);
+
+        LocalDate createCoupleDate = LocalDate.now();
+        coupleService.createCouple(foundInviter, foundReceiver, createCoupleDate);
+        inviteCodeService.removeInviteCodeByCreatedCouple(inviterId, receiverId);
+    }
+
+    @Transactional
     public InviteCodeResponse createInviteCode(Long inviterId, LocalDateTime expireDate) {
         return inviteCodeService.createInviteCode(inviterId, expireDate);
     }
 
     @Transactional
-    public void removeCouple(Long coupleId) {
+    public void removeCouple(LoginMember member) {
+        Long coupleId = member.getCoupleId();
         memberService.breakUpMembersByCoupleId(coupleId);
         coupleService.removeCouple(coupleId);
     }
--- src/main/java/com/musseukpeople/woorimap/couple/application/CoupleService.java
+++ src/main/java/com/musseukpeople/woorimap/couple/application/CoupleService.java
@@ -1,9 +1,15 @@
 package com.musseukpeople.woorimap.couple.application;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.musseukpeople.woorimap.couple.domain.Couple;
 import com.musseukpeople.woorimap.couple.domain.CoupleRepository;
+import com.musseukpeople.woorimap.couple.domain.vo.CoupleMembers;
+import com.musseukpeople.woorimap.member.domain.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,5 +23,12 @@ public class CoupleService {
     @Transactional
     public void removeCouple(Long coupleId) {
         coupleRepository.deleteById(coupleId);
+    }
+
+    @Transactional
+    public Long createCouple(Member inviter, Member receiver, LocalDate createCoupleDate) {
+        CoupleMembers coupleMembers = new CoupleMembers(List.of(inviter, receiver));
+        Couple couple = coupleRepository.save(new Couple(createCoupleDate, coupleMembers));
+        return couple.getId();
     }
 }
