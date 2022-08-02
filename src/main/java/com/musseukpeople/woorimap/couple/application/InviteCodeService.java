472fca87395bdf9559d0420f489e83ed4781e093
Yongsu Kang <dksktm15@naver.com>
2022-08-02 22:13:56 +0900

    [WOOR-70] feat: 커플 초대 코드 조회 및 삭제 구현

--- src/main/java/com/musseukpeople/woorimap/couple/application/InviteCodeService.java
+++ src/main/java/com/musseukpeople/woorimap/couple/application/InviteCodeService.java
@@ -5,9 +5,11 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.musseukpeople.woorimap.common.exception.ErrorCode;
 import com.musseukpeople.woorimap.couple.application.dto.response.InviteCodeResponse;
 import com.musseukpeople.woorimap.couple.domain.InviteCode;
 import com.musseukpeople.woorimap.couple.domain.InviteCodeRepository;
+import com.musseukpeople.woorimap.couple.exception.NotFoundInviteCodeException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,5 +29,15 @@ public class InviteCodeService {
             );
 
         return new InviteCodeResponse(inviteCode.getCode());
+    }
+
+    public Long getInviterIdByInviteCode(String inviteCode) {
+        InviteCode foundInviteCode = inviteCodeRepository.findById(inviteCode)
+            .orElseThrow(() -> new NotFoundInviteCodeException(ErrorCode.NOT_FOUND_INVITE_CODE, inviteCode));
+        return foundInviteCode.getInviterId();
+    }
+
+    public void removeInviteCodeByCreatedCouple(Long inviterId, Long receiverId) {
+        inviteCodeRepository.deleteByTwoMemberId(inviterId, receiverId);
     }
 }
