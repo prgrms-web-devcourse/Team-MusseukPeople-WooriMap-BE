741f591a922bb6dad312ece1579f937a2002c716
yuminhwan <ymh7532@icloud.com>
2022-08-02 13:59:33 +0900

    [WOOR-141] feat: 인가 권한 구현

--- src/main/java/com/musseukpeople/woorimap/auth/domain/login/LoginMember.java
+++ src/main/java/com/musseukpeople/woorimap/auth/domain/login/LoginMember.java
@@ -1,19 +1,43 @@
 package com.musseukpeople.woorimap.auth.domain.login;
 
+import static com.musseukpeople.woorimap.auth.domain.login.LoginMember.Authority.*;
+
 import lombok.Getter;
 
 @Getter
 public class LoginMember {
 
-    private final Long id;
-    private final Long coupleId;
+    private Long id;
+    private Long coupleId;
+    private Authority authority;
+
+    public LoginMember(Authority authority) {
+        this(null, null, authority);
+    }
 
     public LoginMember(Long id, Long coupleId) {
+        this(id, coupleId, coupleId != null ? COUPLE : SOLO);
+    }
+
+    private LoginMember(Long id, Long coupleId, Authority authority) {
         this.id = id;
         this.coupleId = coupleId;
+        this.authority = authority;
+    }
+
+    public boolean isAnonymous() {
+        return ANONYMOUS.equals(authority);
     }
 
     public boolean isCouple() {
-        return this.coupleId != null;
+        return COUPLE.equals(authority);
+    }
+
+    public boolean isSolo() {
+        return SOLO.equals(authority);
+    }
+
+    public enum Authority {
+        ANONYMOUS, SOLO, COUPLE
     }
 }
