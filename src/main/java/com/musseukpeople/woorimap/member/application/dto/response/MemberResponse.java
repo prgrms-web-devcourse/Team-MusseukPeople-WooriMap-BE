0d684d8ff3f1509ca193e676c538ffe8659a1e6c
yuminhwan <ymh7532@icloud.com>
2022-08-04 08:45:21 +0900

    [WOOR-55] feat: DTO swagger 설명 추가

--- src/main/java/com/musseukpeople/woorimap/member/application/dto/response/MemberResponse.java
+++ src/main/java/com/musseukpeople/woorimap/member/application/dto/response/MemberResponse.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import com.musseukpeople.woorimap.member.domain.Member;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +13,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberResponse {
 
+    @Schema(description = "프로필 이미지 url")
     private String imageUrl;
+
+    @Schema(description = "닉네임")
     private String nickName;
+
+    @Schema(description = "커플 상대방 닉네임", nullable = true)
     private String coupleNickName;
+
+    @Schema(description = "커플 시작 날짜", nullable = true)
     private LocalDate coupleStartingDate;
+
+    @Schema(description = "커플 유무")
     private boolean isCouple;
 
     public MemberResponse(String imageUrl, String nickName, String coupleNickName, LocalDate coupleStartingDate,
