376f307fd4b46000f8966b82cecc6915b0ef655a
Yongsu Kang <dksktm15@naver.com>
2022-08-04 15:18:23 +0900

    [WOOR-72] refactor: 커플 수정 Response 스웨거 설명 추가

--- src/main/java/com/musseukpeople/woorimap/couple/application/dto/response/CoupleEditResponse.java
+++ src/main/java/com/musseukpeople/woorimap/couple/application/dto/response/CoupleEditResponse.java
@@ -2,6 +2,7 @@ package com.musseukpeople.woorimap.couple.application.dto.response;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CoupleEditResponse {
 
+    @Schema(description = "수정된 날짜")
     private LocalDate startDate;
 
     public CoupleEditResponse(LocalDate startDate) {
