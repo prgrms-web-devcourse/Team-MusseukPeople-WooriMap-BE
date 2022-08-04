51db4e4642cf83ff920a735f4642d55c524ca9c3
Yongsu Kang <dksktm15@naver.com>
2022-08-04 11:23:27 +0900

    [WOOR-73] refactor: DTO 변환 메소드 이동

--- src/main/java/com/musseukpeople/woorimap/couple/application/dto/response/CoupleMyResponse.java
+++ src/main/java/com/musseukpeople/woorimap/couple/application/dto/response/CoupleMyResponse.java
@@ -1,5 +1,10 @@
 package com.musseukpeople.woorimap.couple.application.dto.response;
 
+import com.musseukpeople.woorimap.common.exception.ErrorCode;
+import com.musseukpeople.woorimap.couple.domain.Couple;
+import com.musseukpeople.woorimap.couple.exception.MappingCoupleMemberException;
+import com.musseukpeople.woorimap.member.domain.Member;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,5 +23,18 @@ public class CoupleMyResponse {
     public CoupleMyResponse(String imageUrl, String nickName) {
         this.imageUrl = imageUrl;
         this.nickName = nickName;
+    }
+
+    public static CoupleMyResponse from(Couple couple, Long myId) {
+        return couple.getCoupleMembers().getMembers().stream()
+            .filter(member -> member.isSame(myId))
+            .findAny()
+            .map(CoupleMyResponse::from)
+            .orElseThrow(
+                () -> new MappingCoupleMemberException("나의 정보를 변환할 수 없습니다.", ErrorCode.NOT_MAPPING_COUPLE_MEMBER));
+    }
+
+    public static CoupleMyResponse from(Member member) {
+        return new CoupleMyResponse(member.getImageUrl(), member.getNickName().getValue());
     }
 }
--- src/main/java/com/musseukpeople/woorimap/couple/application/dto/response/CoupleYourResponse.java
+++ src/main/java/com/musseukpeople/woorimap/couple/application/dto/response/CoupleYourResponse.java
@@ -1,5 +1,10 @@
 package com.musseukpeople.woorimap.couple.application.dto.response;
 
+import com.musseukpeople.woorimap.common.exception.ErrorCode;
+import com.musseukpeople.woorimap.couple.domain.Couple;
+import com.musseukpeople.woorimap.couple.exception.MappingCoupleMemberException;
+import com.musseukpeople.woorimap.member.domain.Member;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,5 +23,18 @@ public class CoupleYourResponse {
     public CoupleYourResponse(String imageUrl, String nickName) {
         this.imageUrl = imageUrl;
         this.nickName = nickName;
+    }
+
+    public static CoupleYourResponse from(Couple couple, Long myId) {
+        return couple.getCoupleMembers().getMembers().stream()
+            .filter(member -> !member.isSame(myId))
+            .findAny()
+            .map(CoupleYourResponse::from)
+            .orElseThrow(
+                () -> new MappingCoupleMemberException("상대방의 정보를 변환할 수 없습니다.", ErrorCode.NOT_MAPPING_COUPLE_MEMBER));
+    }
+
+    public static CoupleYourResponse from(Member member) {
+        return new CoupleYourResponse(member.getImageUrl(), member.getNickName().getValue());
     }
 }
