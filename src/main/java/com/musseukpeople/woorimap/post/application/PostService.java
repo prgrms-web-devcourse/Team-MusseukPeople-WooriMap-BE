f2b0ec83031844c862b6d1f8008bb6f9fec9fcf7
will kim <78planet@gmail.com>
2022-08-04 17:22:39 +0900

    [WOOR-37] refactor: 안쓰는 메서드 삭제

--- src/main/java/com/musseukpeople/woorimap/post/application/PostService.java
+++ src/main/java/com/musseukpeople/woorimap/post/application/PostService.java
@@ -6,8 +6,6 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.musseukpeople.woorimap.common.exception.ErrorCode;
-import com.musseukpeople.woorimap.post.exception.NotFoundPostException;
 import com.musseukpeople.woorimap.couple.domain.Couple;
 import com.musseukpeople.woorimap.post.application.dto.CreatePostRequest;
 import com.musseukpeople.woorimap.post.domain.Post;
@@ -28,11 +26,6 @@ public class PostService {
     public Long createPost(Couple couple, CreatePostRequest createPostRequest, List<Tag> tagsOfPost) {
         Post post = createPostRequest.toPost(couple, toPostTag(tagsOfPost));
         return postRepository.save(post).getId();
-    }
-
-    public Post getPost(Long postId) {
-        return postRepository.findById(postId)
-            .orElseThrow(() -> new NotFoundPostException(ErrorCode.NOT_FOUND_COUPLE, postId));
     }
 
     private List<PostTag> toPostTag(List<Tag> tagsOfPost) {
