442efbd6760fff2c5496e5498a5e4295993896ba
yuminhwan <ymh7532@icloud.com>
2022-08-05 18:42:44 +0900

    [WOOR-172] refactor: 파라미터 네이밍 변경

--- src/main/java/com/musseukpeople/woorimap/tag/infrastructure/QueryTagRepositoryImpl.java
+++ src/main/java/com/musseukpeople/woorimap/tag/infrastructure/QueryTagRepositoryImpl.java
@@ -3,9 +3,11 @@ package com.musseukpeople.woorimap.tag.infrastructure;
 import static com.musseukpeople.woorimap.tag.domain.QTag.*;
 
 import java.util.List;
+
 import org.springframework.stereotype.Repository;
-import com.musseukpeople.woorimap.tag.domain.Tag;
+
 import com.musseukpeople.woorimap.couple.domain.Couple;
+import com.musseukpeople.woorimap.tag.domain.Tag;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -17,11 +19,11 @@ public class QueryTagRepositoryImpl implements QueryTagRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<Tag> findExistTagByCoupleId(Couple couple, List<String> tagNameList) {
+    public List<Tag> findExistTagByCoupleId(Couple couple, List<String> tagNames) {
         return jpaQueryFactory.selectFrom(tag)
             .where(
                 tag.couple.eq(couple),
-                tag.name.in(tagNameList)
+                tag.name.value.in(tagNames)
             )
             .fetch();
     }
