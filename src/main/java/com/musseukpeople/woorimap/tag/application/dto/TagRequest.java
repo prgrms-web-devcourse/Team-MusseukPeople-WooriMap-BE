c77a0c89f9d658ca84a71baef91126c61be38e9c
yuminhwan <ymh7532@icloud.com>
2022-08-05 17:01:59 +0900

    [WOOR-172] refactor: swagger 설명 추가

--- src/main/java/com/musseukpeople/woorimap/tag/application/dto/TagRequest.java
+++ src/main/java/com/musseukpeople/woorimap/tag/application/dto/TagRequest.java
@@ -2,16 +2,21 @@ package com.musseukpeople.woorimap.tag.application.dto;
 
 import javax.validation.constraints.NotBlank;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TagRequest {
 
+    @Schema(description = "태그 이름")
     @NotBlank
     private String name;
 
+    @Schema(description = "태그 색깔")
+    @NotBlank
     private String color;
 
     public TagRequest(String name, String color) {
