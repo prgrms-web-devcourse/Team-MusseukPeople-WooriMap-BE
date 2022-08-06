54f9395af1c8a789e3307c7e0923f9bf37fc9145
yuminhwan <ymh7532@icloud.com>
2022-08-06 17:53:19 +0900

    [WOOR-179] test: 응답값 리스트 변환 메서드 구현

--- src/test/java/com/musseukpeople/woorimap/util/AcceptanceTest.java
+++ src/test/java/com/musseukpeople/woorimap/util/AcceptanceTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -111,6 +112,13 @@ public abstract class AcceptanceTest {
     protected <T> T getResponseObject(MockHttpServletResponse response, Class<T> type) throws IOException {
         JavaType javaType = objectMapper.getTypeFactory().constructParametricType(ApiResponse.class, type);
         ApiResponse<T> result = objectMapper.readValue(response.getContentAsString(), javaType);
+        return result.getData();
+    }
+
+    protected <T> List<T> getResponseList(MockHttpServletResponse response, Class<T> type) throws IOException {
+        JavaType insideType = objectMapper.getTypeFactory().constructParametricType(List.class, type);
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(ApiResponse.class, insideType);
+        ApiResponse<List<T>> result = objectMapper.readValue(response.getContentAsString(), javaType);
         return result.getData();
     }
 
