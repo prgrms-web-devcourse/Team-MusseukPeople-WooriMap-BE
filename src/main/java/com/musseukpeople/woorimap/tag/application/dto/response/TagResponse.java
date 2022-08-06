80cc82f9fb17c198da347603898d574aea8cd23d
yuminhwan <ymh7532@icloud.com>
2022-08-06 17:42:47 +0900

    [WOOR-179] feat: Swagger 설명 추가

--- src/main/java/com/musseukpeople/woorimap/tag/application/dto/response/TagResponse.java
+++ src/main/java/com/musseukpeople/woorimap/tag/application/dto/response/TagResponse.java
@@ -1,5 +1,6 @@
 package com.musseukpeople.woorimap.tag.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TagResponse {
 
+    @Schema(description = "태그 이름")
     private String name;
+
+    @Schema(description = "태그 색깔")
     private String color;
 
     public TagResponse(String name, String color) {
