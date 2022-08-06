424fedc85473fe76017c3919c678ea318e08c95b
yuminhwan <ymh7532@icloud.com>
2022-08-06 17:53:52 +0900

    [WOOR-179] test: 태그 조회 Acceptance 테스트

---
+++ src/main/java/com/musseukpeople/woorimap/tag/presentation/TagController.java
@@ -0,0 +1,32 @@
+package com.musseukpeople.woorimap.tag.presentation;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.musseukpeople.woorimap.auth.aop.OnlyCouple;
+import com.musseukpeople.woorimap.auth.domain.login.Login;
+import com.musseukpeople.woorimap.auth.domain.login.LoginMember;
+import com.musseukpeople.woorimap.common.model.ApiResponse;
+import com.musseukpeople.woorimap.tag.application.dto.response.TagResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "태그", description = "태그 관련 API입니다.")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/couples/tags")
+public class TagController {
+
+    @Operation(summary = "태그 전체 조회", description = "태그 전체 조회 API입니다.")
+    @OnlyCouple
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<TagResponse>>> showAll(@Login LoginMember loginMember) {
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/test/java/com/musseukpeople/woorimap/tag/presentation/TagControllerTest.java
@@ -0,0 +1,65 @@
+package com.musseukpeople.woorimap.tag.presentation;
+
+import static java.util.stream.Collectors.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import com.musseukpeople.woorimap.member.application.dto.request.SignupRequest;
+import com.musseukpeople.woorimap.post.application.dto.CreatePostRequest;
+import com.musseukpeople.woorimap.tag.application.dto.request.TagRequest;
+import com.musseukpeople.woorimap.tag.application.dto.response.TagResponse;
+import com.musseukpeople.woorimap.util.AcceptanceTest;
+
+class TagControllerTest extends AcceptanceTest {
+
+    @DisplayName("태그 조회 성공")
+    @Test
+    void showAll() throws Exception {
+        // given
+        String accessToken = 회원가입_토큰(new SignupRequest("test@gmail.com", "!Hwan1234", "Hwan"));
+        accessToken = 커플_맺기_토큰(accessToken);
+        게시글_작성(accessToken, createPostRequestWithTag("서울", "부산", "맛집"));
+
+        // when
+        MockHttpServletResponse response = mockMvc.perform(get("/api/couples/tags")
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andReturn().getResponse();
+
+        // then
+        List<TagResponse> tagResponses = getResponseList(response, TagResponse.class);
+        assertAll(
+            () -> assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value()),
+            () -> assertThat(tagResponses).hasSize(3)
+        );
+    }
+
+    private CreatePostRequest createPostRequestWithTag(String... names) {
+        List<TagRequest> tagRequests = Arrays.stream(names)
+            .map(name -> new TagRequest(name, "#FFFFFF"))
+            .collect(toList());
+
+        return CreatePostRequest.builder()
+            .title("첫 이야기")
+            .content("<h1>첫 이야기.... </h1>")
+            .imageUrls(List.of("imageUrl1", "imageUrl2"))
+            .tags(tagRequests)
+            .datingDate(LocalDate.now())
+            .latitude(new BigDecimal("12.12312321"))
+            .longitude(new BigDecimal("122.3123121"))
+            .build();
+    }
+}
