bd2ecaba10505ea7f01b49bdc5e664283005556e
dbslzld15 <dbslzld15@naver.com>
2022-08-07 16:40:11 +0900

    [WOOR-121] feat: 이미지 업로드 빈공백 예외처리 테스트 코드 구현

--- src/test/java/com/musseukpeople/woorimap/image/application/S3ImageServiceTest.java
+++ src/test/java/com/musseukpeople/woorimap/image/application/S3ImageServiceTest.java
@@ -2,14 +2,14 @@ package com.musseukpeople.woorimap.image.application;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.ActiveProfiles;
 
 import com.google.common.io.Files;
 import com.musseukpeople.woorimap.common.config.S3MockConfig;
@@ -18,21 +18,17 @@ import com.musseukpeople.woorimap.util.IntegrationTest;
 
 import io.findify.s3mock.S3Mock;
 
-@ActiveProfiles("test")
 @Import(S3MockConfig.class)
 class S3ImageServiceTest extends IntegrationTest {
     @Autowired
     private S3ImageService s3ImageService;
 
-    @Autowired
-    private S3Mock s3Mock;
-
-    @AfterEach
-    public void tearDown() {
+    @AfterAll
+    public static void tearDown(@Autowired S3Mock s3Mock) {
         s3Mock.stop();
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "이미지 파일명 : {0}, 이미지 Content-Type: {1}")
     @CsvSource(value = {"mock.png, image/png", "mock.jpg, image/jpeg",
         "mock.jpeg, image/jpeg", "mock.gif, image/gif", "mock.bmp, image/bmp"})
     @DisplayName("s3 이미지 업로드 성공")
@@ -48,9 +44,23 @@ class S3ImageServiceTest extends IntegrationTest {
         assertThat(imageUrl).contains(extension);
     }
 
+    @ParameterizedTest(name = "이미지 파일명 : {0}, 이미지 Content-Type: {1}")
+    @NullAndEmptySource
+    @DisplayName("빈 공백이 들어왔을경우 실패")
+    void uploadImage_nullOrBlank_fail(String imageName) {
+        //given
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("mock", imageName,
+            "image/jpeg", "test data".getBytes());
+        long memberId = 1L;
+        //when
+        //then
+        assertThatThrownBy(() -> s3ImageService.uploadImage(memberId, mockMultipartFile))
+            .isInstanceOf(NotSupportImageException.class);
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"mock.txt, text/plain", "mock.html, text/html", "mock.pdf, application/pdf"})
-    @DisplayName("s3 지원하지 않는 이미지 확장자일시 실패")
+    @DisplayName("지원하지 않는 이미지 확장자일시 실패")
     void uploadImage_extension_fail(String imageName, String contentType) {
         //given
         MockMultipartFile mockMultipartFile = new MockMultipartFile("mock", imageName,
