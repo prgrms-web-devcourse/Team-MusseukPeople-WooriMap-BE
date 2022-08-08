40646e267f0ee08deecb406c74fdee8ca3ddfc5a
Yongsu Kang <dksktm15@naver.com>
2022-08-08 18:40:40 +0900

    [WOOR-61] feat: 게시글 Repo QueryDsl 추가

--- src/main/java/com/musseukpeople/woorimap/post/domain/PostRepository.java
+++ src/main/java/com/musseukpeople/woorimap/post/domain/PostRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import com.musseukpeople.woorimap.post.infrastructure.PostQueryRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository {
 
     @Query("SELECT p FROM Post p "
         + "JOIN FETCH p.postImages.postImages "
