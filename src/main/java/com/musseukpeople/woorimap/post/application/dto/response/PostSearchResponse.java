f7398d1fd3ea0916dc9ef1bdee1159c98f8a243b
Yongsu Kang <dksktm15@naver.com>
2022-08-09 02:45:13 +0900

    [WOOR-61] refactor: 스웨거 설명 추가 및 생성자 접근자 변경

--- src/main/java/com/musseukpeople/woorimap/post/application/dto/response/PostSearchResponse.java
+++ src/main/java/com/musseukpeople/woorimap/post/application/dto/response/PostSearchResponse.java
@@ -2,11 +2,8 @@ package com.musseukpeople.woorimap.post.application.dto.response;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import com.musseukpeople.woorimap.post.domain.Post;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,32 +13,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PostSearchResponse {
 
+    @Schema(description = "게시글 아이디")
     private Long postId;
-    private String postThumbnailPath;
+
+    @Schema(description = "썸네일 사진")
+    private String imageUrl;
+
+    @Schema(description = "제목")
     private String title;
+
+    @Schema(description = "작성일")
     private LocalDateTime createDateTime;
+
+    @Schema(description = "위도")
     private BigDecimal latitude;
+
+    @Schema(description = "경도")
     private BigDecimal longitude;
 
     @Builder
-    private PostSearchResponse(Long postId, String postThumbnailPath, String title, LocalDateTime createDateTime,
-                               BigDecimal latitude, BigDecimal longitude) {
+    public PostSearchResponse(Long postId, String imageUrl, String title, LocalDateTime createDateTime,
+                              BigDecimal latitude, BigDecimal longitude) {
         this.postId = postId;
-        this.postThumbnailPath = postThumbnailPath;
+        this.imageUrl = imageUrl;
         this.title = title;
         this.createDateTime = createDateTime;
         this.latitude = latitude;
         this.longitude = longitude;
-    }
-
-    public static List<PostSearchResponse> from(List<Post> posts) {
-        return posts.stream().map(post -> new PostSearchResponse(
-            post.getId(),
-            post.getTitle(),
-            post.getThumbnailUrl(),
-            post.getCreatedDateTime(),
-            post.getLocation().getLatitude(),
-            post.getLocation().getLongitude()
-        )).collect(Collectors.toList());
     }
 }
