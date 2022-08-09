59fce353e7bbff384fa59abe51f04e8cf5e72c48
yuminhwan <ymh7532@icloud.com>
2022-08-09 18:23:03 +0900

    [WOOR-63] feat: 게시물 응답 DTO 구현

--- src/main/java/com/musseukpeople/woorimap/post/application/dto/response/PostResponse.java
+++ src/main/java/com/musseukpeople/woorimap/post/application/dto/response/PostResponse.java
@@ -1,11 +1,17 @@
 package com.musseukpeople.woorimap.post.application.dto.response;
 
+import static java.util.stream.Collectors.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.musseukpeople.woorimap.tag.application.dto.response.PostTagResponse;
+import com.musseukpeople.woorimap.post.domain.Post;
+import com.musseukpeople.woorimap.post.domain.tag.PostTag;
+import com.musseukpeople.woorimap.post.domain.vo.Location;
+import com.musseukpeople.woorimap.tag.domain.Tag;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,16 +30,38 @@ public class PostResponse {
     private LocalDate datingDate;
     private LocalDate createdDate;
 
-    public PostResponse(Long id, String title, String content, List<String> imageUrls, LocalDate createdDate,
-                        LocalDate datingDate, LocationResponse location, List<PostTagResponse> tags) {
+    public PostResponse(Long id, String title, String content, List<String> imageUrls, LocationResponse location,
+                        List<PostTagResponse> tags, LocalDate createdDate, LocalDate datingDate) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.imageUrls = imageUrls;
-        this.createdDate = createdDate;
-        this.datingDate = datingDate;
         this.location = location;
         this.tags = tags;
+        this.datingDate = datingDate;
+        this.createdDate = createdDate;
+    }
+
+    public static PostResponse from(Post post) {
+        List<PostTagResponse> tags = toPostTagResponse(post);
+
+        return new PostResponse(
+            post.getId(),
+            post.getTitle(),
+            post.getContent(),
+            post.getImageUrls(),
+            LocationResponse.from(post.getLocation()),
+            tags,
+            post.getCreatedDateTime().toLocalDate(),
+            post.getDatingDate()
+        );
+    }
+
+    private static List<PostTagResponse> toPostTagResponse(Post post) {
+        return post.getPostTags().getPostTags().stream()
+            .map(PostTag::getTag)
+            .map(PostTagResponse::from)
+            .collect(toList());
     }
 
     @Getter
@@ -45,6 +73,33 @@ public class PostResponse {
         public LocationResponse(BigDecimal latitude, BigDecimal longitude) {
             this.latitude = latitude;
             this.longitude = longitude;
+        }
+
+        public static LocationResponse from(Location location) {
+            return new LocationResponse(
+                location.getLatitude(),
+                location.getLongitude()
+            );
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    static class PostTagResponse {
+
+        @Schema(description = "태그 이름")
+        private String name;
+
+        @Schema(description = "태그 색깔")
+        private String color;
+
+        public PostTagResponse(String name, String color) {
+            this.name = name;
+            this.color = color;
+        }
+
+        public static PostTagResponse from(Tag tag) {
+            return new PostTagResponse(tag.getName(), tag.getColor());
         }
     }
 }
