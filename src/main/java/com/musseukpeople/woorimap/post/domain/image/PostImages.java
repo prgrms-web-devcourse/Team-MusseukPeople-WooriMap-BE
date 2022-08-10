9d6f922051abc7b486aaf7f723ec31ba73317ba7
will kim <45508756+78planet@users.noreply.github.com>
2022-08-10 12:58:58 +0900

    [WOOR-222] fix: post 생성 시 태그와 이미지 validation 추가 (#57)
    * [WOOR-222] fix: post 생성 시 태그와 이미지 validation 추가
    
    * [WOOR-222] fix: post 생성 시 태그와 이미지 validation 테스트 추가

--- src/main/java/com/musseukpeople/woorimap/post/domain/image/PostImages.java
+++ src/main/java/com/musseukpeople/woorimap/post/domain/image/PostImages.java
@@ -1,6 +1,7 @@
 package com.musseukpeople.woorimap.post.domain.image;
 
 import static java.util.stream.Collectors.*;
+import static com.google.common.base.Preconditions.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +20,17 @@ import lombok.NoArgsConstructor;
 public class PostImages {
 
     public static final int THUMBNAIL_INDEX = 0;
+    private static final int POST_IMAGES_MIN_SIZE = 1;
+    private static final int POST_IMAGES_MAX_SIZE = 5;
+    private static final String POST_IMAGES_MIN_SIZE_MESSAGE = "이미지는 1개 이상 등록해야합니다";
+    private static final String POST_IMAGES_MAX_SIZE_MESSAGE = "이미지는 최대 5개 등록 가능합니다";
+
     @OneToMany(mappedBy = "post", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<PostImage> postImages = new ArrayList<>();
 
     public PostImages(List<PostImage> postImages) {
+        checkArgument(postImages.size() >= POST_IMAGES_MIN_SIZE, POST_IMAGES_MIN_SIZE_MESSAGE);
+        checkArgument(postImages.size() <= POST_IMAGES_MAX_SIZE, POST_IMAGES_MAX_SIZE_MESSAGE);
         this.postImages = postImages;
     }
 
@@ -37,6 +45,8 @@ public class PostImages {
     }
 
     public void changePostImages(List<PostImage> postImagesToAdd) {
+        checkArgument(postImages.size() >= POST_IMAGES_MIN_SIZE, POST_IMAGES_MIN_SIZE_MESSAGE);
+        checkArgument(postImages.size() <= POST_IMAGES_MAX_SIZE, POST_IMAGES_MAX_SIZE_MESSAGE);
         postImages.clear();
         postImages.addAll(postImagesToAdd);
     }
