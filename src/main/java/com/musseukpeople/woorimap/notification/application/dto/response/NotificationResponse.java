1e98d95c003b9cbf930f4ee02adadebc984598cc
yuminhwan <ymh7532@icloud.com>
2022-08-12 19:34:45 +0900

    [WOOR-250] feat: 알림 응답용 DTO 구현

---
+++ src/main/java/com/musseukpeople/woorimap/notification/application/dto/response/NotificationResponse.java
@@ -0,0 +1,33 @@
+package com.musseukpeople.woorimap.notification.application.dto.response;
+
+import com.musseukpeople.woorimap.notification.domain.Notification;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class NotificationResponse {
+
+    private Long id;
+    private Long contentId;
+    private String nickName;
+    private String content;
+
+    public NotificationResponse(Long id, Long contentId, String nickName, String content) {
+        this.id = id;
+        this.contentId = contentId;
+        this.nickName = nickName;
+        this.content = content;
+    }
+
+    public static NotificationResponse from(Notification notification) {
+        return new NotificationResponse(
+            notification.getId(),
+            notification.getContentId(),
+            String.valueOf(notification.getSenderId()),
+            notification.getContent()
+        );
+    }
+}
