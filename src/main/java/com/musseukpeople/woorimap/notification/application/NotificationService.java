116dd62263637dc8912a8fe3e90d793bf75e921e
yuminhwan <ymh7532@icloud.com>
2022-08-12 19:35:19 +0900

    [WOOR-250] feat: 알림 전달 기능 구현

--- src/main/java/com/musseukpeople/woorimap/notification/application/NotificationService.java
+++ src/main/java/com/musseukpeople/woorimap/notification/application/NotificationService.java
@@ -1,13 +1,20 @@
 package com.musseukpeople.woorimap.notification.application;
 
+import static com.musseukpeople.woorimap.notification.domain.Notification.*;
 import static java.lang.String.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.musseukpeople.woorimap.event.domain.PostEvent;
+import com.musseukpeople.woorimap.notification.application.dto.response.NotificationResponse;
 import com.musseukpeople.woorimap.notification.domain.EmitterRepository;
+import com.musseukpeople.woorimap.notification.domain.Notification;
+import com.musseukpeople.woorimap.notification.domain.NotificationRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,35 +22,69 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class NotificationService {
 
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
     private static final String CONNECT_EVENT_NAME = "connect check";
     private static final String CONNECT_EVENT_MESSAGE_FORMAT = "EventStream Created. [userId= %d]";
+    private static final String ID_DELIMITER = "_";
 
     private final EmitterRepository emitterRepository;
+    private final NotificationRepository notificationRepository;
 
     // TODO : lastEventId로 유실된 이벤트 전송해야 함
     public SseEmitter subscribe(Long memberId, String lastEventId) {
-        String emitterId = memberId + "_" + System.currentTimeMillis();
+        String emitterId = createEmitterId(memberId);
         SseEmitter sseEmitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
 
         sseEmitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
         sseEmitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
 
-        sendNotification(sseEmitter, CONNECT_EVENT_NAME, emitterId, format(CONNECT_EVENT_MESSAGE_FORMAT, memberId));
+        sendConnectNotification(sseEmitter, emitterId, memberId);
         return sseEmitter;
     }
 
-    private void sendNotification(SseEmitter sseEmitter, String eventName, String emitterId, Object data) {
+    public void sendPostNotification(PostEvent postEvent) {
+        Notification notification = notificationRepository.save(createNotification(postEvent));
+        String id = notification.getReceiverId() + ID_DELIMITER;
+        String eventId = createEmitterId(notification.getReceiverId());
+        Map<String, SseEmitter> emitters = emitterRepository.findAllStartWithById(id);
+        emitters.forEach(
+            (key, emitter) -> sendNotification(emitter, eventId, notification.getEventName(), key,
+                NotificationResponse.from(notification))
+        );
+    }
+
+    private void sendConnectNotification(SseEmitter sseEmitter, String emitterId, Long memberId) {
+        sendNotification(sseEmitter, emitterId, CONNECT_EVENT_NAME, emitterId,
+            format(CONNECT_EVENT_MESSAGE_FORMAT, memberId));
+    }
+
+    private void sendNotification(SseEmitter sseEmitter, String eventId, String eventName, String emitterId,
+                                  Object data) {
         try {
             sseEmitter.send(SseEmitter.event()
-                .id(emitterId)
+                .id(eventId)
                 .name(eventName)
                 .data(data));
         } catch (IOException e) {
             emitterRepository.deleteById(emitterId);
             log.info("이벤트 전송을 실패했습니다. emitterId : {}", emitterId);
         }
+    }
+
+    private Notification createNotification(PostEvent postEvent) {
+        return Notification.builder()
+            .senderId(postEvent.getSourceId())
+            .receiverId(postEvent.getDestinationId())
+            .contentId(postEvent.getPostId())
+            .content(postEvent.getContent())
+            .type(NotificationType.valueOf(postEvent.getEventType().toString()))
+            .build();
+    }
+
+    private String createEmitterId(Long memberId) {
+        return memberId + ID_DELIMITER + System.currentTimeMillis();
     }
 }
--- src/test/java/com/musseukpeople/woorimap/notification/application/NotificationServiceTest.java
+++ src/test/java/com/musseukpeople/woorimap/notification/application/NotificationServiceTest.java
@@ -1,18 +1,34 @@
 package com.musseukpeople.woorimap.notification.application;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.musseukpeople.woorimap.event.domain.PostEvent;
+import com.musseukpeople.woorimap.event.domain.PostEvent.EventType;
+import com.musseukpeople.woorimap.notification.domain.EmitterRepository;
 import com.musseukpeople.woorimap.util.IntegrationTest;
 
+@ExtendWith(MockitoExtension.class)
 class NotificationServiceTest extends IntegrationTest {
 
     @Autowired
     private NotificationService notificationService;
+
+    @SpyBean
+    private EmitterRepository emitterRepository;
 
     @DisplayName("알림 구독 성공")
     @Test
@@ -25,6 +41,26 @@ class NotificationServiceTest extends IntegrationTest {
         SseEmitter sseEmitter = notificationService.subscribe(id, lastEventId);
 
         // then
-        assertThat(sseEmitter).isNotNull();
+        assertAll(
+            () -> assertThat(sseEmitter).isNotNull(),
+            () -> then(emitterRepository).should(times(1)).save(anyString(), any())
+        );
+    }
+
+    @DisplayName("알림 메시지 전송 성공")
+    @Test
+    void sendPostNotification() throws IOException {
+        // given
+        Long receiverId = 2L;
+        SseEmitter mockSseEmitter = mock(SseEmitter.class);
+        PostEvent postEvent = new PostEvent(1L, receiverId, 1L, EventType.POST_CREATED, "test", LocalDateTime.now());
+        given(emitterRepository.findAllStartWithById(anyString())).willReturn(
+            Map.of(String.valueOf(receiverId), mockSseEmitter));
+
+        // when
+        notificationService.sendPostNotification(postEvent);
+
+        // then
+        then(mockSseEmitter).should(times(1)).send(any());
     }
 }
