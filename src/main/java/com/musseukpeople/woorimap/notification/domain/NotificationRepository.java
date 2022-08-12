8bc6bbece4df49db2be9730b9e4fa87e669e6d2e
yuminhwan <ymh7532@icloud.com>
2022-08-13 01:01:43 +0900

    [WOOR-249] feat: 읽지 않은 알림 조회 쿼리 구현

--- src/main/java/com/musseukpeople/woorimap/notification/domain/NotificationRepository.java
+++ src/main/java/com/musseukpeople/woorimap/notification/domain/NotificationRepository.java
@@ -1,6 +1,13 @@
 package com.musseukpeople.woorimap.notification.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    @Query("SELECT n FROM Notification n WHERE n.receiverId = :id AND n.isRead = false ")
+    List<Notification> findUnreadAllByMemerId(@Param("id") Long id);
 }
