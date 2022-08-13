2ad9900976ec32df2930dab9b5974acc066eeb0e
Minhwan Yu <ymh7532@icloud.com>
2022-08-13 14:25:35 +0900

    [WOOR-250] feat: 발행된 이벤트 전달 기능 구현 (#65)
    * [WOOR-250] feat: 알림 엔티티 구현
    
    * [WOOR-250] refactor: 클래스명 변경
    
    * [WOOR-250] feat: 이벤트 이름 반환 기능 구현
    
    * [WOOR-250] feat: 회원이 접속한 모든 SseEmitter 조회 기능 구현
    
    * [WOOR-250] feat: 알림 응답용 DTO 구현
    
    * [WOOR-250] feat: 알림 전달 기능 구현
    
    * [WOOR-250] feat: sub로 받은 이벤트 알림 전송 기능 구현
    
    * [WOOR-250] refactor: 채널 이름 변경
    
    * [WOOR-250] feat: Redis 채널 구독 기능 구현
    
    * [WOOR-250] feat: 패키지 분리
    
    * [WOOR-250] refactor: 채널명 post로 고정하도록 변경
    
    * [WOOR-250] test: mock 테스트 시 컨텍스트 다시 띄우도록 변경
    
    * [WOOR-250] feat: 유실 데이터 저장 기능 구현
    
    * [WOOR-250] refactor: 이벤트 이름 지정 삭제 및 유실 데이터 전송 기능 구현
    
    * [WOOR-250] refactor: 메서드 명, 파라미터 명 수정
    
    * [WOOR-250] refactor: 예외 메시지 수정
    
    * [WOOR-250] refactor: 패키지 이동
    
    * [WOOR-250] test: 테스트 깨짐 수정
    
    * [WOOR-250] feat: 회원의 모든 SseEmitter, Event 삭제 기능 구현
    
    * [WOOR-250] feat: 로그아웃 시 모든 SseEmitter, Event 삭제
    
    * [WOOR-250] style: TODO 처리
    
    * [WOOR-250] test: 테스트 깨짐 수정
    
    * [WOOR-250] test: 테스트 케이스 추가

--- src/main/java/com/musseukpeople/woorimap/common/config/redis/RedisConfig.java
+++ src/main/java/com/musseukpeople/woorimap/common/config/redis/RedisConfig.java
@@ -8,16 +8,19 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-import com.musseukpeople.woorimap.notification.application.RedisMessageSubscriber;
+import com.musseukpeople.woorimap.notification.application.message.RedisMessageSubscriber;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
 
+    private static final ChannelTopic POST_CHANNEL = ChannelTopic.of("post");
+    
     private final String host;
     private final String password;
     private final int port;
@@ -41,9 +44,11 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory,
+                                                                       RedisMessageSubscriber redisMessageSubscriber) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(redisMessageSubscriber, POST_CHANNEL);
         return container;
     }
 
---
+++ src/main/java/com/musseukpeople/woorimap/notification/domain/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.musseukpeople.woorimap.notification.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
